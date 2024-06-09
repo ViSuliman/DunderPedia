3a6cb57c37dca2dd506644a21f5f58532a4291d7
ViSuliman <vitoria.suliman@sptech.school>
2024-06-09 14:24:25 -0300

    estilizações e página minha historia

--- bd_duderpedia.sql
+++ bd_duderpedia.sql
@@ -21,17 +21,24 @@ tipo varchar(50));
 create table resultado (
 fkUsuario int,
 fkQuiz int,
-data_hora datetime,
+data_hora datetime DEFAULT CURRENT_TIMESTAMP,
 valor int,
 foreign key (fkUsuario) references usuario(idUsuario),
 foreign key (fkQuiz) references quiz(idQuiz),
-primary key (fkUsuario, fkQuiz)
+primary key (fkUsuario, fkQuiz, data_hora)
 );
+
 
 insert into quiz (idQuiz,tipo) values
 (1,'Conhecimento'),
 (2,'Personalidade');
 
-
+SELECT usuario.nome, MAX(resultado.valor) as media_acertos
+FROM resultado
+JOIN usuario ON resultado.fkUsuario = usuario.idUsuario
+WHERE resultado.fkQuiz = 1
+GROUP BY usuario.nome
+ORDER BY media_acertos DESC
+LIMIT 5;
 
 
