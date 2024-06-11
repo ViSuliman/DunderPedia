a814814eb257cf8c662b134d1f6d1ac327c9fa5d
ViSuliman <vitoria.suliman@sptech.school>
2024-06-11 01:05:00 -0300

    versão com melhorias na dashboard e telas concluidas

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// var ambiente_processo = 'producao';
-var ambiente_processo = 'desenvolvimento';
+var ambiente_processo = 'producao';
+// var ambiente_processo = 'desenvolvimento';
 
 var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
 // Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
@@ -16,11 +16,9 @@ var HOST_APP = process.env.APP_HOST;
 var app = express();
 
 var indexRouter = require("./src/routes/index");
-var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
+var usuarioRouter = require("./src/routes/usuarios")
 var resultadoRouter = require("./src/routes/resultado");
-var aquariosRouter = require("./src/routes/aquarios");
-var empresasRouter = require("./src/routes/empresas");
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -30,10 +28,8 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/avisos", avisosRouter);
 app.use("/resultado", resultadoRouter);
-app.use("/aquarios", aquariosRouter);
-app.use("/empresas", empresasRouter);
+
 
 app.listen(PORTA_APP, function () {
     console.log(`
